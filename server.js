204704f28bd79759b5e5b3c2e483f744f4f9f565
Jesse Wei <jessew13@email.unc.edu>
2023-03-09 21:18:39 -0500

    Test

--- server.js
+++ server.js
@@ -3,31 +3,33 @@
 import {rps, rpsls} from "./lib/rpsls.js"
 import minimist from 'minimist'
 import express from 'express'
+import bodyParser from 'body-parser'
 
 var argv = minimist(process.argv.slice(2));
 const port = argv.port || 5000;
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/app', (req, res) => {
     res.status(200).send('200 OK').end();
 });
 
 app.get('/app/rps', (req, res) => {
-    res.status(200).send(JSON.stringify(rps())).end();
+    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
 })
 
 app.get('/app/rpsls', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls())).end();
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
 })
 
 app.post('/app/rps/play', (req, res) => {
-    console.log(req.body);
     res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
 })
 
 app.post('/app/rpsls/play', (req, res) => {
-    console.log(req.body);
     res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
 })
 
