3e1036ec2266e393230dbe0658cf8934054f4e6c
Jesse Wei <jessew13@email.unc.edu>
2023-03-10 06:03:36 -0500

    Test

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var argv = minimist(process.argv.slice(2));
 const port = argv.port || 5000;
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
@@ -24,52 +23,40 @@ app.get('/app/rpsls', (req, res) => {
     res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
 })
 
+/**
+ * URLEncoded
+ */
 app.get('/app/rps/play', (req, res) => {
     res.status(200).send(JSON.stringify(rps(req.query.shot))).end();
 })
 
+/**
+ * URLEncoded
+ */
 app.get('/app/rpsls/play', (req, res) => {
     res.status(200).send(JSON.stringify(rps(req.query.shot))).end();
 })
 
+/**
+ * JSON
+ */
 app.post('/app/rps/play', (req, res) => {
     res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
 })
 
+/**
+ * JSON
+ */
 app.post('/app/rpsls/play', (req, res) => {
     res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
 })
 
-app.get('/app/rps/play/rock', (req, res) => {
-    res.status(200).send(JSON.stringify(rps('rock'))).end();
+app.get('/app/rps/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.params['shot']))).end();
 })
 
-app.get('/app/rps/play/paper', (req, res) => {
-    res.status(200).send(JSON.stringify(rps('paper'))).end();
-})
-
-app.get('/app/rps/play/scissors', (req, res) => {
-    res.status(200).send(JSON.stringify(rps('scissors'))).end();
-})
-
-app.get('/app/rpsls/play/rock', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls('rock'))).end();
-})
-
-app.get('/app/rpsls/play/paper', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls('paper'))).end();
-})
-
-app.get('/app/rpsls/play/scissors', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls('scissors'))).end();
-})
-
-app.get('/app/rpsls/play/lizard', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls('lizard'))).end();
-})
-
-app.get('/app/rpsls/play/spock', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls('spock'))).end();
+app.get('/app/rpsls/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.params['shot']))).end();
 })
 
 app.get('*', (req, res) => {
