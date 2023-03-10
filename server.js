a70787d6b132ea59ca5164d16525a32462796a88
Jesse Wei <jessew13@email.unc.edu>
2023-03-10 09:05:20 -0500

    Documentation

--- server.js
+++ server.js
@@ -15,16 +15,23 @@ app.get('/app', (req, res) => {
     res.status(200).send('200 OK').end();
 });
 
+/**
+ * Random rps shot
+ */
 app.get('/app/rps', (req, res) => {
     res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
 })
 
+/**
+ * Random rpsls shot
+ */
 app.get('/app/rpsls', (req, res) => {
     res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
 })
 
 /**
  * URLEncoded
+ * Note: This is a GET
  */
 app.get('/app/rps/play', (req, res) => {
     res.status(200).send(JSON.stringify(rps(req.query.shot))).end();
@@ -32,6 +39,7 @@ app.get('/app/rps/play', (req, res) => {
 
 /**
  * URLEncoded
+ * Note: This is a GET
  */
 app.get('/app/rpsls/play', (req, res) => {
     res.status(200).send(JSON.stringify(rps(req.query.shot))).end();
@@ -39,6 +47,7 @@ app.get('/app/rpsls/play', (req, res) => {
 
 /**
  * JSON
+ * Note: This is a POST
  */
 app.post('/app/rps/play', (req, res) => {
     res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
@@ -46,26 +55,30 @@ app.post('/app/rps/play', (req, res) => {
 
 /**
  * JSON
+ * Note: This is a POST
  */
 app.post('/app/rpsls/play', (req, res) => {
     res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
 })
 
 /**
- * Get the shot field of the endpoint
+ * /:shot syntax allows for that field to be parsed
  */
 app.get('/app/rps/play/:shot', (req, res) => {
     res.status(200).send(JSON.stringify(rps(req.params.shot))).end();
 })
 
 /**
- * Get the shot field of the endpoint
+ * /:shot syntax allows for that field to be parsed
  */
 app.get('/app/rpsls/play/:shot', (req, res) => {
     res.status(200).send(JSON.stringify(rpsls(req.params.shot))).end();
 })
 
-app.get('*', (req, res) => {
+/**
+ * Default (non-existent) endpoint
+ */
+app.all('*', (req, res) => {
     res.status(404).send('404 NOT FOUND').end();
 })
 
