c498d64573cb29bca946da48d298188d22dbdc94
Jesse Wei <jessew13@email.unc.edu>
2023-03-09 20:51:16 -0500

    Add /app/rps/play/... and /app/rpsls/play/... endpoints

--- server.js
+++ server.js
@@ -21,4 +21,44 @@ app.get('/app/rpsls', (req, res) => {
     res.status(200).send(JSON.stringify(rpsls())).end();
 })
 
+app.get('/app/rps/play', (req, res) => {
+
+})
+
+app.get('/app/rpsls/play', (req, res) => {
+
+})
+
+app.get('/app/rps/play/rock', (req, res) => {
+    res.status(200).send(JSON.stringify(rps('rock'))).end();
+})
+
+app.get('/app/rps/play/paper', (req, res) => {
+    res.status(200).send(JSON.stringify(rps('paper'))).end();
+})
+
+app.get('/app/rps/play/scissors', (req, res) => {
+    res.status(200).send(JSON.stringify(rps('scissors'))).end();
+})
+
+app.get('/app/rpsls/play/rock', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls('rock'))).end();
+})
+
+app.get('/app/rpsls/play/paper', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls('paper'))).end();
+})
+
+app.get('/app/rpsls/play/scissors', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls('scissors'))).end();
+})
+
+app.get('/app/rpsls/play/lizard', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls('lizard'))).end();
+})
+
+app.get('/app/rpsls/play/spock', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls('spock'))).end();
+})
+
 app.listen(port);
