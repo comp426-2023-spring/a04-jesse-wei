4dfc3937a86b45005d22f4d24722ee7385c0cbef
Jesse Wei <jessew13@email.unc.edu>
2023-03-09 19:11:16 -0500

    Test

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 #!/usr/bin/env node
 
 import {rps, rpsls} from "./lib/rpsls.js"
+import minimist from 'minimist'
+import express from 'express'
 
+var argv = minimist(process.argv.slice(2));
+const port = argv.port || 5000;
+
+const app = express();
+
+app.get('/app', (err, res) => {
+    res.status(200);
+    res.end();
+});
+
+app.get('/app/rps', (err, res) => {
+    return rps();
+})
