b57076fde94c776be18d83efdc327f28d640683f
Jesse Wei <jessew13@email.unc.edu>
2023-03-09 21:13:08 -0500

    Add testing stuff

--- server.js
+++ server.js
@@ -22,10 +22,12 @@ app.get('/app/rpsls', (req, res) => {
 })
 
 app.post('/app/rps/play', (req, res) => {
+    console.log(req.body);
     res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
 })
 
 app.post('/app/rpsls/play', (req, res) => {
+    console.log(req.body);
     res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
 })
 
